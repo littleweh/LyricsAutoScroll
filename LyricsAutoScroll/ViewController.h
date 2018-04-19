6e220bd33888aa7dbe7e7d596e08d8227fb9378c
Ada Kao <adakao@zdev.local>
2018-04-19 15:01:46 +0800

    Initial Commit

---
+++ LyricsAutoScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LyricsAutoScroll
+//
+//  Created by Ada Kao on 19/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
