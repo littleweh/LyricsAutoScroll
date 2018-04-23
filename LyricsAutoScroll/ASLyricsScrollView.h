219a76b9d867040588184f32b523b0da09f4696d
Ada Kao <adakao@zdev.local>
2018-04-23 15:28:24 +0800

    add a ASLyricsScrollView for MVC

---
+++ LyricsAutoScroll/ASLyricsScrollView.h
@@ -0,0 +1,13 @@
+//
+//  ASLyricsScrollView.h
+//  LyricsAutoScroll
+//
+//  Created by Ada Kao on 23/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASLyricsScrollView : UIScrollView
+
+@end
---
+++ LyricsAutoScroll/ASLyricsScrollView.m
@@ -0,0 +1,21 @@
+//
+//  ASLyricsScrollView.m
+//  LyricsAutoScroll
+//
+//  Created by Ada Kao on 23/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import "ASLyricsScrollView.h"
+
+@implementation ASLyricsScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
